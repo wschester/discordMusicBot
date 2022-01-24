3aee82073793e7f370f4d163aff74e3effbe73e3
Wschester <48420074+wschester@users.noreply.github.com>
2022-01-24 21:03:40 +1100

    fixed queue

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -7,8 +7,9 @@ module.exports = {
 
 		if (!queue) return message.channel.send('There is nothing in the queue right now!');
 
-		const q = queue.songs.map((song, i) => `${i === 0 ? 'Playing:' : `${i}.`} \`${song.name} - ${song.formattedDuration}\``).join('\n');
+		const q = queue.songs.slice(0, 6).map((song, i) => `${i === 0 ? 'Playing:' : `${i}.`} \`${song.name} - ${song.formattedDuration}\``).join('\n');
 
-		message.channel.send(`${global.client.emotes.queue}**Server Queue**${global.client.emotes.queue}\n${q}`);
+		message.channel.send(`${global.client.emotes.queue}**Server Queue**${global.client.emotes.queue}\n${q}
+			${queue.songs.length >= 6 ? 'More in the queue...' : ''}`);
 	},
 };
